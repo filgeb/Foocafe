833183b6d3e651b906354ef147ffda68ae03e5e6
filgeb <51028788+filgeb@users.noreply.github.com>
2019-08-15 16:35:39 +0200

    Add files via upload

---
+++ JavaScript1/week 03/strings.js
@@ -0,0 +1,9 @@
+// 1.1
+let myString = 'hello,this,is,a,difficult,to,read,sentence';
+console.log(myString);
+//1.2
+console.log('the length of my string is:' + myString.length);
+//1.3
+let myNewString = myString.replace(/,/g, ' ');
+//1.4
+console.log(myNewString);
